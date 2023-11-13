da09987889a575bdb423daadf613dd4af78717a2
ruotonglinzylin <linr691@newschool.edu>
2023-11-13 18:01:27 -0500

    Initial commit

---
+++ 1.js
@@ -0,0 +1,33 @@
+  let rad = 60; // I changed the width of the shape
+  let xpos, ypos;
+
+  let xspeed = 6; // I changed the speed of the shape
+  let yspeed = 6;
+
+  let xdirection = 3;
+  let ydirection = 3;
+
+  function setup() {
+    createCanvas(720, 400);
+    noStroke();
+    frameRate(30);
+    ellipseMode(RADIUS);
+    // I changed the starting position of the shape
+    xpos = width / 2;
+    ypos = height / 2;
+  }
+
+  function draw() {
+    background(100,7);// I changed the color and added a "path" behind the circle
+
+    xpos = xpos + xspeed * xdirection;
+    ypos = ypos + yspeed * ydirection;
+
+    if (xpos > width - rad || xpos < rad) {
+        xdirection *= -1;}
+    if (ypos > height - rad || ypos < rad) {
+        ydirection *= -1;
+    }
+
+    ellipse(xpos, ypos, rad, rad);
+  }
